7ab836d8c5f5e84a44b267d01c504e714a126605
Eduardo Melo <eduardo.melo01992@gmail.com>
2022-07-25 15:26:22 -0300

    Backend project created

---
+++ backend/src/main/java/com/asmeduardo/dsclient/DsclientApplication.java
@@ -0,0 +1,13 @@
+package com.asmeduardo.dsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/asmeduardo/dsclient/DsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asmeduardo.dsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
